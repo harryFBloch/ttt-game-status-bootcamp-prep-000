849820df2f2641ba0a7aa2acd0f229a16ed1610f
Harry Bloch <harryfbloch@gmail.com>
2018-09-03 20:52:03 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,39 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  
+  def won(board)
+    returnBool = false
+    WIN_COMBINATIONS.each do |combo|
+      pos1 = board[combo[0]]
+      pos2 = board[combo[1]]
+      pos3 = board[combo[2]]
+      puts "TEST#{pos1}, #{pos2}, #{pos3}TEST"
+      if pos1 == pos2 && pos2 == pos3 && pos3 == pos1
+        puts "Combo = #{combo}TEST"
+        return combo
+      end
+    end
+    return returnBool
+  end
+  
+
+  
+   def full_board(board)
+    board.all? do |space|
+       if space != "X" || space != "O"
+       end
+    end 
+   end
+  
+  won([" ", " ", " "," ", " ", " "," ", "X", "X"])
